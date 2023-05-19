43bbd371946e834f7815bd4389be6b23d66981a0
Alexander Bondarenko <aleksandrrechica@gmail.com>
2023-05-20 01:13:00 +0300

    DI

--- VectorGraphic/VectorGraphicViewer/App.xaml.cs
+++ VectorGraphic/VectorGraphicViewer/App.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using VectorGraphicViewer.PrimitivesProviders;
+using VectorGraphicViewer.ViewModels;
 
 namespace VectorGraphicViewer
 {
@@ -13,5 +16,23 @@ namespace VectorGraphicViewer
     /// </summary>
     public partial class App : Application
     {
+        private ServiceProvider serviceProvider;
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton<IPrimitivesProvider>(new JSONPrimitiveProvider("Primitives.json"));
+            services.AddSingleton<MainWindow>();
+            services.AddSingleton<MainVM>();
+        }
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
     }
 }
